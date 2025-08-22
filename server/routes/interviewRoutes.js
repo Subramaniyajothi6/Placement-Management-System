725e5a05e22629a9b2626bbd6490650a22218896
Subramaniya Jothi <subramaniyajothi2002@gmail.com>
2025-08-22 13:21:07 +0530

    change in backend

--- server/routes/interviewRoutes.js
+++ server/routes/interviewRoutes.js
@@ -4,12 +4,12 @@ const { authMiddleware, roleMiddleware } = require('../middleware/authMiddleware
 
 const interviewRouter = express.Router();
 
-interviewRouter.post('/',authMiddleware,roleMiddleware(['company,admin']),createInterview);
+interviewRouter.post('/',authMiddleware,roleMiddleware(['company','admin']),createInterview);
 interviewRouter.get('/',authMiddleware,roleMiddleware(['admin']),getAllInterviews);
-interviewRouter.get('/:id',authMiddleware,getSingleInterview);
-interviewRouter.put('/:id',authMiddleware,roleMiddleware(['company,admin']),updateInterview);
-interviewRouter.delete('/:id',authMiddleware,roleMiddleware(['company,admin']),deleteInterview);
 interviewRouter.get('/my',authMiddleware,roleMiddleware(['student']),getMyInterviews)
+interviewRouter.get('/:id',authMiddleware,getSingleInterview);
+interviewRouter.put('/:id',authMiddleware,roleMiddleware(['company','admin']),updateInterview);
+interviewRouter.delete('/:id',authMiddleware,roleMiddleware(['company','admin']),deleteInterview);
 
 
 
