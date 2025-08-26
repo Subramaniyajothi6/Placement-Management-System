de844ed39193e0f08d7f641749c3e43347045e6c
Subramaniya Jothi <subramaniyajothi2002@gmail.com>
2025-08-26 23:39:05 +0530

    change in backend

--- server/routes/reportRouter.js
+++ server/routes/reportRouter.js
@@ -2,10 +2,10 @@ const express = require('express');
 const { createReport, getAllReports, getReportById, updateReport, deleteReport } = require('../controllers/reportController');
 const reportRouter = express.Router();
 
-router.post('/', createReport);
-router.get('/', getAllReports);
-router.get('/:id', getReportById);
-router.put('/:id', updateReport);
-router.delete('/:id', deleteReport);
+reportRouter.post('/', createReport);
+reportRouter.get('/', getAllReports);
+reportRouter.get('/:id', getReportById);
+reportRouter.put('/:id', updateReport);
+reportRouter.delete('/:id', deleteReport);
 
 module.exports = reportRouter;
