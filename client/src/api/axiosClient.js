f0c61e586166700d65ae94a4eb10570c846745a0
Subramaniya Jothi <subramaniyajothi2002@gmail.com>
2025-09-10 16:27:54 +0530

    minor fixes

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    // baseURL: 'https://placement-management-system-2d32.onrender.com/api/v1',
-    baseURL: 'http://localhost:3000/api/v1',
+    baseURL: 'https://placement-management-system-2d32.onrender.com/api/v1',
+    // baseURL: 'http://localhost:3000/api/v1',
     headers: {
         'Content-Type': 'application/json',
     },
